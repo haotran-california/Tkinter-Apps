6fdcd7394e29540e5c3c6cfc848396021c286913
Ogriceboy <haoanhtran7@gmail.com>
2022-01-10 18:33:11 -0800

    part 2

--- Text-Editor-App/textEditor.py
+++ Text-Editor-App/textEditor.py
@@ -6,6 +6,57 @@ window = Tk()
 window.title("Text Editor")
 window.geometry("1200x660")
 
+#DEFINE COMANDS
+def newFile(): 
+    mainText.delete("1.0", END)
+    window.title("New File")
+    statusBar.config(text="New File  ")
+
+def openFile(): 
+    mainText.delete("1.0", END)
+    textFile = filedialog.askopenfilename(initialdir="C:/", title="Open File", filetypes=(("Text Files", "*.txt"), ("Python Files", "*.py"), ("All Files", ("*."))))
+
+    #Update Status Bars
+    name = textFile
+    statusBar.config(text=f'{name}')
+    name = name.replace("C:/gui/", "")
+    window.title(f'{name} - Textpad')
+
+    #Open file 
+    textFile = open(textFile, 'r')
+    text = textFile.read()
+    mainText.insert(END, text)
+    textFile.close()
+
+def saveFile(): 
+    return 0
+
+def saveFileAs(): 
+    textFile = filedialog.asksaveasfilename(defaultextension=".*", initialdir="C:/", title="Save File", filetypes=(("Text Files", "*.txt"), ("Python Files", "*.py"), ("All Files", ("*."))))
+    if textFile: 
+        #Update status bars
+        name = textFile
+        name = name.replace("C:/", "")
+        statusBar.config(text=f'Saved: {name}')
+        window.title(f'{name} - Textpad')
+
+        #Save file 
+        textFile = open(textFile, 'w')
+        textFile.write(mainText.get("1.0", END))
+        textFile.close()
+
+# def copyEdit(): 
+#     return 0
+
+# def cutEdit(): 
+#     return 0
+
+# def undoEdit(): 
+#     return 0
+
+# def redoEdit(): 
+#     return 0
+
 #Frame 
 mainFrame = Frame(window)
 mainFrame.pack(pady=10)
@@ -26,24 +77,24 @@ window.config(menu=mainMenu)
 
 #File Menu 
 fileMenu = Menu(mainMenu, tearoff=False)
-fileMenu.add_cascade(label="File", menu=fileMenu)
-fileMenu.add_command(label="New")
-fileMenu.add_command(label="Open")
-fileMenu.add_command(label="Save")
+mainMenu.add_cascade(label="File", menu=mainMenu)
+fileMenu.add_command(label="New", command=newFile)
+fileMenu.add_command(label="Open", command=openFile)
+fileMenu.add_command(label="Save", command=saveFile)
+fileMenu.add_command(label="Save As", command=saveFileAs)
 fileMenu.add_separator()
 fileMenu.add_command(label="Exit")
 
 #Edit Menu
 editMenu = Menu(mainMenu, tearoff=False)
-editMenu.add_cascade(label="Cut")
-editMenu.add_cascade(label="Copy")
-editMenu.add_cascade(label="Undo")
-editMenu.add_cascade(label="Redo")
+mainMenu.add_cascade(label="Edit", menu=mainMenu)
+editMenu.add_command(label="Cut")
+editMenu.add_command(label="Copy")
+editMenu.add_command(label="Undo")
+editMenu.add_command(label="Redo")
 
 #Status Bar
-statusBar = Label(window, text="Ready... ", anchor=E)
+statusBar = Label(window, text="Ready        ", anchor=E)
 statusBar.pack(fill=X, side=BOTTOM, ipady=5)
-
-
 
 window.mainloop()

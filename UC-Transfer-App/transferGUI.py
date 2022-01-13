328b5ca844030d77a175b1c26e5646ea289d3049
Ogriceboy <haoanhtran7@gmail.com>
2022-01-13 13:15:22 -0800

    reformatted results window

--- UC-Transfer-App/transferGUI.py
+++ UC-Transfer-App/transferGUI.py
@@ -18,35 +18,29 @@ def newWindow(fn):
     def openResultWindow(): 
         query = fn()
         display = Toplevel()
+
+        #parse query into variables 
         college = query[1]
         major = query[2]
         admitGPA = query[3]
         admitRate = query[4]
         enrollGPA = query[5]
-        yeildRate = query[6]
         admits = query[7]
         applicants = query[8]
-        enrolls = query[9]
 
-        collegeLbl = Label(master = display, text=college)
-        majorLbl = Label(master = display, text=major)
-        admitGPALbl = Label(master = display, text=admitGPA)
-        admitRateLbl = Label(master = display, text=admitRate)
-        enrollGPALbl = Label(master = display, text=enrollGPA)
-        yeildRateLbl = Label(master = display, text=yeildRate)
-        admitsLbl = Label(master = display, text = admits)
-        applicantsLbl = Label(master = display, text=applicants)
-        enrollsLbl = Label(master = display, text =enrolls)
+        description = ["College:", "Major:", "Admit GPA:", "Admit Rate:", "Enroll GPA:", "Admits:", "Applicants:"]
+        data = [college, major, admitGPA, admitRate, enrollGPA, admits, applicants]
 
-        collegeLbl.pack()
-        majorLbl.pack()
-        admitGPALbl.pack()
-        admitRateLbl.pack()
-        enrollGPALbl.pack()
-        yeildRateLbl.pack()
-        admitsLbl.pack()
-        applicantsLbl.pack()
-        enrollsLbl.pack()
+        #display 
+        for i in range(len(data)): 
+            descriptionLbl = Label(master=display, text=description[i])
+            dataLbl = Label(master=display, text=data[i])
+            descriptionLbl.grid(row=i, column=0, sticky=W)
+            dataLbl.grid(row=i, column=1)
+
+        exitBtn = Button(master=display, text="Exit", command=display.quit)
+        exitBtn.grid(row = 7, column=0, sticky=W)
+
         return query
 
     return openResultWindow
@@ -61,18 +55,21 @@ def submitSearch():
 schools = ["UCB", "UCD", "UCI", "UCLA", "UCR", "UCSB", "UCSC", "UCSD"]
 
 #MAIN PROGRAM WIDGETS 
-selectSchool = ttk.Combobox(window, value=schools)
+selectSchool = ttk.Combobox(window, value=schools, state="readonly")
 selectSchool.current(0)
 selectSchool.bind("<<ComboboxSelected>>", comboSelect)
 selectSchool.grid(row=1, column=0)
 
 majors = loadSelectMajor(selectSchool.get())
 
-selectMajor = ttk.Combobox(window, value=majors)
+selectMajor = ttk.Combobox(window, value=majors, state="readonly")
 selectMajor.current(0)
 selectMajor.grid(row=2, column=0)
 
 submitBtn = Button(text="Submit", command=submitSearch)
-submitBtn.grid(row=3, column=0)
+submitBtn.grid(row=3, column=0, sticky=W)
+
+exitBtn = Button(text="Exit", command=window.quit)
+exitBtn.grid(row = 3, column=1, sticky=W)
 
 window.mainloop()
